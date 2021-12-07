34b5a1e00b9878d5ed45ec1476453cfdcd732049
AnanthTrebuh <xignir@gmail.com>
2021-12-07 08:50:34 +0100

    Create Player.h

---
+++ Yams/Yams/Player.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <cstdlib>
+#include <iostream>
+#include "Figure.h"
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+class Player {
+	int _id; 
+	std::string _name;
+	unsigned int _score;
+	std::vector<Figure> _figures;
+	std::vector<std::string> _figureToDo;
+};
+#endif
